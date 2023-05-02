e512842f416aed34405853bf658d37188776736f
Harshit Jain <108606500+jain-harshit03@users.noreply.github.com>
2023-05-02 23:58:02 +0530

    Add files via upload

---
+++ JAVA_PROECT/TIMETABLE_MANAGEMENT/timetablee.java
@@ -0,0 +1,16 @@
+package TIMETABLE_MANAGEMENT;
+
+public class timetablee
+{
+    String a[][] = new String[6][8];
+    private String batch_name;
+
+    void setBatch_name(String s)
+    {
+        this.batch_name = s;
+    }
+    String getBatch_name()
+    {
+        return this.batch_name;
+    }
+}
